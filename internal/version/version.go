8bf85cef88522f9fff27bd85ef312b5fceaa8c6f
iamminhquan <minhquan542005@gmail.com>
2026-04-16 21:30:20 +0700

    feat: add `version` for the tool

---
+++ internal/version/version.go
@@ -0,0 +1,7 @@
+// Package version holds the build-time version string.
+package version
+
+// Version is injected at build time via ldflags:
+//
+//	go build -ldflags "-X github.com/iamminhquan/gotodo/internal/version.Version=1.2.3" ./cmd/gotodo
+var Version = "dev"
