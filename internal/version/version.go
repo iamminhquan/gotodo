30c220f474fa58413059721b08204cc8626787d7
iamminhquan <minhquan542005@gmail.com>
2026-04-21 09:41:36 +0700

    chore(release): prepare v1.0.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,4 +4,4 @@ package version
 // Version is injected at build time via ldflags:
 //
 //	go build -ldflags "-X github.com/iamminhquan/gotodo/internal/version.Version=1.2.3" ./cmd/gotodo
-var Version = "dev"
+var Version = "v1.0.0"
